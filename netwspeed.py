a68ac3181cf50433181fb8589ec61143e234b0d7
akshim1702 <73992286+akshim1702@users.noreply.github.com>
2021-07-12 18:24:57 +0530

    Update netwspeed.py

--- netwspeed.py
+++ netwspeed.py
@@ -2,7 +2,7 @@ import speedtest
 
 wifi = speedtest.Speedtest()
 
-ping = print(wifi.results.ping)
+ping = print('Ping:- ',wifi.results.ping,' ms')
 download = wifi.download()
 upload = wifi.upload()
 print('Download Speed :- ',round(download/(10**6)/2),'mb/s')
